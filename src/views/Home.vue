b28ba20f2bc2309e21e05f561077ddfb74aa403e
JonSnow <1571298579@qq.com>
2019-12-09 16:57:53 +0800

    测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<template lang="html">
+  <div class="">
+    121212
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  watch: {},
+  components: {}
 };
 </script>
+
+<style lang="scss" scoped></style>
