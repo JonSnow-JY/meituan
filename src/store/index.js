c3d92129f13c602a3d29a93370c1a51a610d92ba
JonSnow <1571298579@qq.com>
2019-12-10 11:15:08 +0800

    vuex练习

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,52 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import * as types from "./mutation-types";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    num: 1,
+    age: 18,
+    list: [
+      {
+        name: "JonSnow",
+        age: 18
+      },
+      {
+        name: "Cercei",
+        age: 19
+      },
+      {
+        name: "LittleFinguer",
+        age: 20
+      }
+    ]
+  },
+  getters: {
+    // listLen: state => [...state.list.filter(item => item > 2)],
+    // longListLen: (state, getters) => [...getters.listLen, state.num, state.age]
+    getNameByAge: state => age => state.list.find(item => item.age === age).name
+  },
+  mutations: {
+    [types.ADD_NUM](state, params) {
+      state.num += params;
+    },
+    addAge(state) {
+      state.age += 10;
+    },
+    reduceNum(state) {
+      state.num -= 2;
+    },
+    reduceAge(state) {
+      state.age -= 10;
+    }
+  },
+  actions: {
+    [types.ADD_AGE]({ commit }) {
+      commit("addAge");
+    }
+  },
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,59 @@
 <template lang="html">
   <div class="">
-    <div class="" v-m="2"></div>
-    <div class="" v-n="2"></div>
+    <div class="">
+      {{ aliasNum }}
+    </div>
+    <div class="">
+      {{ age }}
+    </div>
+    <div class="">
+      {{ totalNum }}
+    </div>
+    <div class="">
+      <!-- {{ newLongListLen }} -->
+      {{ getNameByAge(18) }}
+    </div>
+    <button class="" @click="addNum(1000)">增加num数据</button>
+    <button class="" @click="reduceNum">减少num数据</button>
+    <button class="" @click="addAge">增加age数据</button>
+    <button class="" @click="reduceAge">减少age数据</button>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations, mapGetters, mapActions } from "vuex";
 export default {
   data() {
     return {};
   },
-  computed: {},
+  computed: {
+    ...mapState({
+      aliasNum: "num",
+      age: state => state.age,
+      totalNum(state) {
+        return state.age + ":" + state.num;
+      }
+    }),
+    ...mapGetters(["getNameByAge"])
+  },
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    ...mapMutations(["reduceNum", "reduceAge"]),
+    ...mapMutations({
+      addNum: "ADD_NUM"
+    }),
+    ...mapActions({
+      addAge: "ADD_AGE"
+    })
+    // addNum(num) {
+    //   // this.$store.commit("addNum", params);
+    //   this.$store.commit({
+    //     type: "addNum",
+    //     num
+    //   });
+    // }
+  },
   watch: {},
   components: {}
 };
